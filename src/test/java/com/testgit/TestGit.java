525e43482320b31842f5661671dbc206d44f214f
jagadeeshwarm <malagamjagdesh@gmail.com>
2024-08-22 17:04:59 +0530

    This is commit from copy demo project

---
+++ src/test/java/com/testgit/TestGit.java
@@ -0,0 +1,18 @@
+package com.testgit;
+
+public class TestGit {
+
+	public void gitmethod1() {
+		System.out.println("test1");
+		System.out.println("test2");
+		System.out.println("test3");
+		System.out.println("test4");
+	}
+	
+	public void gitmethod2() {
+		System.out.println("git1");
+		System.out.println("git2");
+		System.out.println("git3");
+		System.out.println("git4");
+	}
+}
